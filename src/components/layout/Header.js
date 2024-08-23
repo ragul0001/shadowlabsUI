285487b2276086717e8f499ef44cce64022ab2d3
ragul0001 <ragulkumar0244@gmail.com>
2024-08-24 00:29:27 +0530

    adding Navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import logo from '../../assests/logo/logo.png'
+import home from '../../assests/Menu-Icons/home.png';
+import App from '../../assests/Menu-Icons/app.png';
+import Game from '../../assests/Menu-Icons/game.png';
+import Gamification from '../../assests/Menu-Icons/game.png';
+import Ar from '../../assests/Menu-Icons/ar-vr.png';
+import web from '../../assests/Menu-Icons/web3.0.png';
+import Blog from '../../assests/Menu-Icons/blog.png';
+import contact from '../../assests/Menu-Icons/contact.png';
+
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,11 +20,11 @@ const Header = () => {
   };
 
   return (
-    <nav className="bg-white shadow-md fixed w-full z-10">
-      <div className="container mx-auto lg:px-8 max-w-7xl px-4 py-3 flex justify-between items-center">
+    <nav className="bg-[#0c0e1a] shadow-md fixed w-full z-10">
+      <div className="container mx-auto lg:px-8 max-w-7xl px-4 py-10 flex justify-between items-center">
         {/* Logo */}
         <div className="text-2xl font-bold">
-          <a href="/">Logo</a>
+          <img src={logo} alt='the logo' />
         </div>
 
         {/* Hamburger Menu Icon */}
@@ -41,9 +51,9 @@ const Header = () => {
         <div
           className={`${
             isOpen ? 'translate-x-0' : 'translate-x-full'
-          } fixed inset-y-0 right-0 w-64 bg-white transform transition-transform duration-300 ease-in-out md:static md:flex md:w-auto md:translate-x-0`}
+          } fixed inset-y-0 right-0 w-64 bg-[#0c0e1a] transform transition-transform duration-300 ease-in-out md:static md:flex md:w-auto md:translate-x-0`}
         >
-          <div className="flex flex-col md:flex-row md:items-center md:space-x-6 p-4 md:p-0">
+          <div className="flex text-[14px] text-[#fff] flex-col md:flex-row md:items-center md:space-x-10 p-4 md:p-0">
             {/* Close Button */}
             <div className="self-end mb-4 md:hidden">
               <button onClick={() => setIsOpen(false)} className="focus:outline-none">
@@ -70,7 +80,13 @@ const Header = () => {
                 activeLink === 'home' ? 'underline text-blue-600' : ''
               }`}
             >
-              Home
+                <ul className='flex  flex-row lg:flex-col list-none items-center'>
+                    <li>
+                      <img src={home} alt='home icon'/>
+                    </li>
+                    <li>HOME</li>
+                </ul>
+            
             </a>
             <a
               href="#"
@@ -79,7 +95,68 @@ const Header = () => {
                 activeLink === 'about' ? 'underline text-blue-600' : ''
               }`}
             >
-              About
+             <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={App} alt='app icon'/>
+                    </li>
+                    <li>APP</li>
+                </ul>
+            </a>
+            <a
+              href="#"
+              onClick={() => handleLinkClick('about')}
+              className={`text-lg font-medium hover:text-gray-600 transition duration-300 ease-in-out transform hover:scale-105 ${
+                activeLink === 'about' ? 'underline text-blue-600' : ''
+              }`}
+            >
+               <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={Game} alt='home icon'/>
+                    </li>
+                    <li>GAME</li>
+                </ul>
+            </a>
+            <a
+              href="#"
+              onClick={() => handleLinkClick('about')}
+              className={`text-lg font-medium hover:text-gray-600 transition duration-300 ease-in-out transform hover:scale-105 ${
+                activeLink === 'about' ? 'underline text-blue-600' : ''
+              }`}
+            >
+              <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={Gamification} alt='home icon'/>
+                    </li>
+                    <li>GAMIFICATION</li>
+                </ul>
+            </a>
+            <a
+              href="#"
+              onClick={() => handleLinkClick('about')}
+              className={`text-lg font-medium hover:text-gray-600 transition duration-300 ease-in-out transform hover:scale-105 ${
+                activeLink === 'about' ? 'underline text-blue-600' : ''
+              }`}
+            >
+                <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={Ar} alt='home icon'/>
+                    </li>
+                    <li>AR/VR</li>
+                </ul>
+            </a>
+            <a
+              href="#"
+              onClick={() => handleLinkClick('about')}
+              className={`text-lg font-medium hover:text-gray-600 transition duration-300 ease-in-out transform hover:scale-105 ${
+                activeLink === 'about' ? 'underline text-blue-600' : ''
+              }`}
+            >
+               <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={web} alt='home icon'/>
+                    </li>
+                    <li>WEB 3.0</li>
+                </ul>
             </a>
             <a
               href="#"
@@ -88,7 +165,12 @@ const Header = () => {
                 activeLink === 'services' ? 'underline text-blue-600' : ''
               }`}
             >
-              Services
+               <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={Blog} alt='home icon'/>
+                    </li>
+                    <li>BLOG</li>
+                </ul>
             </a>
             <a
               href="#"
@@ -97,7 +179,12 @@ const Header = () => {
                 activeLink === 'contact' ? 'underline text-blue-600' : ''
               }`}
             >
-              Contact
+               <ul className='flex flex-col list-none items-center'>
+                    <li>
+                      <img src={contact} alt='home icon'/>
+                    </li>
+                    <li>CONTACT</li>
+                </ul>
             </a>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 }
