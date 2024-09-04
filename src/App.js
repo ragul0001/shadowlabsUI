cf3dacd5a2d485e1a6df7e3272268d966c89deea
ragul0001 <ragulkumar0244@gmail.com>
2024-09-04 23:16:10 +0530

    Particles added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Rough from './pages/Rough'
 import Explore from './pages/Explore';
+import Particles from './pages/Particles';
+import ARexplore from './pages/ARexplore';
+
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore/>}/>
           <Route path="/route" element={<Rough/>}/>
+          <Route path="/particle" element={<Particles/>}/>
+          <Route path="/ar" element={<ARexplore/>}/>
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/Particles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ParticlesComponent from '../components/features/ParticlesComponent'
+
+
+function Particles() {
+  return (
+    <>
+    <div className=' h-screen bg-black'>
+      <ParticlesComponent id="particles"/>
+    </div>
+    </>
+  )
+}
+
+export default Particles
