4131ce349aacd62a6d4870d916d400706bd2645c
ragul0001 <ragulkumar0244@gmail.com>
2024-09-05 21:40:34 +0530

    added

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -27,51 +27,37 @@ function Explore() {
           <div className="flex justify-end basis-6/12">
             <div className="p-4">
               <div className="flex justify-end gap-3">
-                <button
-                  onClick={handleShowVideo}
-                  className="border px-4 border-white hover:bg-blue-700 rounded-full py-3"
-                >
-                  Show Videos
-                </button>
+                  {/* <button
+                    onClick={handleShowVideo}
+                    className="border px-4 border-white hover:bg-blue-700 rounded-full py-3"
+                  >
+                    Show Videos
+                  </button> */}
               </div>
-              <div className="my-10">
-                <h1 className="text-[30px] font-semibold">CGI Creations</h1>
-                <p className="font-thin">
-                  The UK's leading CGI and Animation agency.
-                </p>
-                <p className="mt-10 text-justify w-96">
-                  Spark's CGI studio uses the latest cinematic detailed 3D
-                  software to help create visually stunning environments, CGI
-                  products, and character animations across all sectors. We
-                  deliver exceptional detail and ultra realism for our clients.
-                  Our talented team deliver exceptional quality CGI and
-                  animation for all projects at the most cost effective agency
-                  rates available.
-                </p>
-                {/* <div className="grid grid-cols-4 gap-4 mt-10">
-                  <img
-                    src="https://new.spark-lab.co.uk/img/f58442e0-3f06-4f74-86d1-0de0d74fec91/samsung-logo.png"
-                    className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl"
-                  />
-                  <img
-                    src="https://new.spark-lab.co.uk/img/20a300b8-7bf6-4cdc-b67a-c01ed799e2ab/o2logo.png"
-                    className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl"
-                  />
-                  <img
-                    src="https://new.spark-lab.co.uk/img/1ba68a65-9c28-41b3-9539-c72c77709da0/porsche.png"
-                    className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl"
-                  />
-                  <img
-                    src="https://new.spark-lab.co.uk/img/f6759832-bb01-4903-ac0c-03b4a0b43deb/accora.png"
-                    className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl"
-                  />
-                </div> */}
-              </div>
+              <div className="relative my-10">
+  <div className="absolute inset-0 bg-black opacity-70 z-[-1]"></div>
+  <div className="relative p-3 bg-transparent">
+    <h1 className="text-[30px] font-semibold">CGI Creations</h1>
+    <p className="font-thin">
+      The UK's leading CGI and Animation agency.
+    </p>
+    <p className="mt-10 text-justify w-96">
+      Spark's CGI studio uses the latest cinematic detailed 3D software to help create visually stunning environments, CGI products, and character animations across all sectors. We deliver exceptional detail and ultra realism for our clients. Our talented team deliver exceptional quality CGI and animation for all projects at the most cost effective agency rates available.
+    </p>
+    {/* <div className="grid grid-cols-4 gap-4 mt-10">
+      <img src="https://new.spark-lab.co.uk/img/f58442e0-3f06-4f74-86d1-0de0d74fec91/samsung-logo.png" className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl" />
+      <img src="https://new.spark-lab.co.uk/img/20a300b8-7bf6-4cdc-b67a-c01ed799e2ab/o2logo.png" className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl" />
+      <img src="https://new.spark-lab.co.uk/img/1ba68a65-9c28-41b3-9539-c72c77709da0/porsche.png" className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl" />
+      <img src="https://new.spark-lab.co.uk/img/f6759832-bb01-4903-ac0c-03b4a0b43deb/accora.png" className="w-20 h-20 border p-4 bg-white border-blue-500 rounded-sm shadow-2xl" />
+    </div> */}
+  </div>
+</div>
+
             </div>
           </div>
         </div>
 
-        {/* Background Video */}
+        {/* 1 Background Video */}
         {/* {!showVideo && (
           <div className="absolute z-10 w-screen min-w-full min-h-full max-w-none">
             <ReactPlayer
@@ -85,7 +71,7 @@ function Explore() {
           </div>
         )} */}
 
-        {/* Fullscreen Video */}
+        {/*2 Fullscreen Video */}
         {/* {showVideo && (
           <div className="fixed inset-0 z-50 bg-black bg-opacity-90 flex items-center justify-center">
             <button
