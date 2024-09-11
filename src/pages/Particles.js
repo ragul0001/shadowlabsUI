cffaa183b115345cabab4b869288fb7db46f8285
ragul0001 <ragulkumar0244@gmail.com>
2024-09-11 22:23:30 +0530

    changes in gamification

--- src/pages/Particles.js
+++ src/pages/Particles.js
@@ -44,6 +44,17 @@ function Particles() {
               : "translate-x-0 opacity-100"
           }`}
         >
+          <div className="flex justify-end gap-3">
+            <button
+              onClick={handleShowVideo}
+              class="my-3 group relative min-h-[50px] w-40 overflow-hidden border border-[#3c66ee]  text-white shadow-2xl transition-all rounded-full before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full"
+            >
+              <span class="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
+              <span class="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-white">
+                Show Videos
+              </span>
+            </button>
+          </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2 items-center">
             <div>
               {currentMedia.type === "image" ? (
@@ -56,6 +67,7 @@ function Particles() {
                 <ReactPlayer
                   url={currentMedia.src}
                   playing
+                  controls
                   width="100%"
                   height="100%"
                   className="video-class  "
@@ -63,20 +75,9 @@ function Particles() {
               )}
             </div>
             <div className="p-4">
-              <div className="flex justify-end gap-3">
-                <button
-                  onClick={handleShowVideo}
-                  class="my-3 group relative min-h-[50px] w-40 overflow-hidden border border-[#3c66ee]  text-white shadow-2xl transition-all rounded-full before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full"
-                >
-                  <span class="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
-                  <span class="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-white">
-                    Show Videos
-                  </span>
-                </button>
-              </div>
               <div className="relative my-10">
                 <div className="absolute inset-0 bg-[#12182a]  "></div>
-                <div className="relative p-8 bg-transparent h-[400px] md:h-[700px] lg:h-[290px] overflow-y-scroll">
+                <div className="relative p-8 bg-transparent h-[400px] md:h-[700px] lg:h-[370px] overflow-y-scroll">
                   <h1 className="text-[30px] font-semibold">Gamifications</h1>
                   <div className="mt-10">
                     <h1 className="text-[20px] font-semibold">
