e733d749e8c07d0eb74676a2de802aed26e1ec40
PrabhaKrishnamoorthy <lakshmiprabha41295@gmail.com>
2024-10-04 14:08:21 +0530

    changes made in the background videos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -131,7 +131,7 @@ function Home() {
     <div className="flex justify-end items-center h-full">
       <div className="px-4 md:px-3 lg:px-0 py-5 max-w-lg">
         
-        <div className="mx-0 md:mx-5">
+        <div className="mx-0 md:mx-5 pl-32">
           <div>
             <h1 className="text-[30px] text-white font-regular">
               Welcome to Endorphins Studios
@@ -280,17 +280,20 @@ function Home() {
       </div> */}
 
       {/* Gamifications */}
-      <section id="gamification" className="bg-cover bg-center min-h-screen flex items-center justify-center" style={{ backgroundImage: `url(${gamification})` }}>
+      <section
+        id="gamification"
+        className="bg-cover bg-center min-h-screen flex items-center justify-center"
+        style={{ backgroundImage: `url(${gamification})` }}>
       {/* <div className="bg-[#0C0E1A]" id="gamification" > */}
         <div className="container min-h-screen overflow-hidden flex justify-center items-center mx-auto max-w-7xl lg:px-8">
           <div className="flex justify-end items-center h-full">
             {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-2 items-center px-4 md:px-3 lg:px-0 py-5"> */}
-            <div className="w-full md:w-1/2 px-4 md:px-3 lg:px-0 py-5">
+            <div className="w-full md:w-1/2 px-4 md:px-3 lg:px-0">
               {/* <div className="">
                 <img src={gamification} alt="the content image" className="" />
               </div> */}
 
-              <div>
+             
                 <div>
                   <h1 className="text-[30px] text-white font-regular">
                     Gamifications
@@ -313,7 +316,7 @@ function Home() {
                     </button>
                   </Link>
                 </div>
-              </div>
+              
             </div>
           </div>
         </div>
@@ -433,9 +436,8 @@ function Home() {
   </header>
 </div> */}
 
-<div className="bg-[#0c0e1a] relative" id="ar">
-  <div className="relative w-full h-screen">
-    {/* Video embedded inside the section */}
+
+  {/* <div className="relative w-full h-screen" id="ar">
     <ReactPlayer
       url={AR_Development}
       width="100%"
@@ -445,8 +447,6 @@ function Home() {
       muted
       className="absolute top-0 left-0 w-full h-full object-cover z-0"
     />
-
-    {/* Content section layered on top of the video */}
     <div className="relative z-10 flex justify-center items-center h-full">
       <div className="container mx-auto px-4 lg:px-8 text-white">
         <div className="w-full md:w-1/2">
@@ -472,7 +472,62 @@ function Home() {
         </div>
       </div>
     </div>
+  </div> */}
+
+<div className="bg-[#0c0e1a] relative" id="ar-section">
+  {/* Mobile video player */}
+  <div className="block lg:hidden w-full min-w-full min-h-full max-w-none">
+    <ReactPlayer
+      url={AR_Development}
+      width="100%"
+      height="100%"
+      playing
+      loop
+      muted
+    />
   </div>
+
+  <header className="relative flex justify-end items-center lg:h-screen overflow-hidden">
+    {/* Content container aligned to the right */}
+    <div className="container mx-auto max-w-7xl lg:px-8 relative z-30 text-white rounded-xl transition-all duration-500">
+      <div className="flex justify-start items-center h-full">
+        <div className="w-full md:w-1/2 px-4 md:px-3 lg:px-0 py-5">
+          <div>
+            <h1 className="text-[30px] text-white font-regular">VR Development</h1>
+            <p className="text-[14px] text-white font-light my-5 text-justify">
+              Our VR development team creates fully immersive, realistic
+              virtual environments that transport users to entirely new
+              worlds. Whether for gaming, training, or virtual tours, we
+              deliver high-quality VR solutions that engage and inspire,
+              leveraging the latest in virtual reality technology.
+            </p>
+
+            <Link to="../vr">
+                    {" "}
+                    <button class="my-3 group relative min-h-[50px] w-40 overflow-hidden border border-[#3c66ee]  text-white shadow-2xl transition-all rounded-full before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full">
+                      <span class="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#3c66ee] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#3c66ee] after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
+                      <span class="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-white">
+                        Explore
+                      </span>
+                    </button>
+                  </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* Video Background for larger screens */}
+    <div className="absolute z-10 hidden lg:block w-screen min-w-full min-h-full max-w-none">
+      <ReactPlayer
+        url={AR_Development}
+        width="100%"
+        height="100%"
+        playing
+        loop
+        muted
+      />
+    </div>
+  </header>
 </div>
 
 
